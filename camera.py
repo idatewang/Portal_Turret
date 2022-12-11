d87bcd256b6058eae9e3cb154f9216c61c5a5090
idatewang <idatewang@tamu.eu>
2022-12-11 00:41:07 -0600

    12/11/22

--- camera.py
+++ camera.py
@@ -7,13 +7,15 @@ from imutils.video.pivideostream import PiVideoStream
 import imutils
 import time
 import numpy as np
+import RPi.GPIO as GPIO
 
 class VideoCamera(object):
     def __init__(self, flip = False):
         self.vs = PiVideoStream().start()
         self.flip = flip
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        time.sleep(2.0)
+        self.x_og = 0
+        self.y_og = 0
 
     def __del__(self):
         self.vs.stop()
@@ -23,13 +25,27 @@ class VideoCamera(object):
             return np.flip(frame, 0)
         return frame
 
-    def get_frame(self):
+    def get_frame(self,servo1,servo_pos):
         frame = self.flip_if_needed(self.vs.read())
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        ret, jpeg = cv2.imencode('.jpg', frame)        
+        if(len(faces)>0):
+            print("Detected.")
+            for (x, y, w, h) in faces:
+                if self.x_og == 0 and self.y_og == 0:
+                    self.x_og = x
+                    self.y_og = y
+                if servo_pos != servo_pos+0.05266*(x-self.x_og) and (x-self.x_og<10) and (x-self.x_og>-10):                    
+                    servo_pos = servo_pos+0.05266*(x-self.x_og)
+                    servo1.ChangeDutyCycle(servo_pos)
+                    time.sleep(1)
+                    print(servo_pos)
+                    print("Change in x:",str(x-self.x_og))
+                    print("Change in y:",str(y-self.y_og))
+                self.x_og = x
+                self.y_og = y
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        ret, jpeg = cv2.imencode('.jpg', np.flip(frame, 0))        
         return jpeg.tobytes()
 
     def get_frame_test(self):
--- imutils_flask.py
+++ imutils_flask.py
@@ -8,6 +8,7 @@ from camera import VideoCamera
 import time
 import threading
 import os
+import RPi.GPIO as GPIO
 
 pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
 
@@ -20,8 +21,19 @@ def index():
 
 def gen(camera):
     #get camera frame
+    # Set GPIO numbering mode
+    GPIO.setmode(GPIO.BOARD)
+    # Set pin 11 as an output, and set servo1 as pin 11 as PWM
+    GPIO.setup(11,GPIO.OUT)
+    servo1 = GPIO.PWM(11,50) # Note 11 is pin, 50 = 50Hz pulse
+    #start PWM running, but with value of 0 (pulse off)
+    servo_pos = 7
+    print ("Waiting for 2 seconds")
+    time.sleep(2.0)
+    #servo1.start(7)
+
     while True:
-        frame = camera.get_frame()
+        frame = camera.get_frame(servo1,servo_pos)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
